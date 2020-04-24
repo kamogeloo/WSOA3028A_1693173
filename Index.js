7a3d275b4774a83200869c540e54346babcec505
kamogelomafaesa <kamogelomafaesa7@gmail.com>
2020-04-24 11:36:52 +0200

    changes

--- Index.js
+++ Index.js
@@ -1,3 +1,10 @@
-/*let button = document.createElement("Button");*/
+/*let button = document.createElement("Button");
 const button = document.body.children[4];
 console.log(button);
+
+button.addEventListener('click', function(e){
+    function sayHello(name , role){
+        document.write(arguments[0] + "is"+ arguments[1]  );
+    }
+    sayHello("Naledi", "Queen");
+});*/
